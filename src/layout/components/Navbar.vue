6871a964a0e61b52c157bec2cb9a207f2c471e3f
whtli <3239349119@qq.com>
2022-11-09 11:32:27 +0800

    跳过权限登录验证和退出跳转路径

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -52,8 +52,9 @@ export default {
       this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
-      await this.$store.dispatch('user/logout')
-      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      // await this.$store.dispatch('user/logout')
+      // this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      this.$router.push(`/login`)
     }
   }
 }
